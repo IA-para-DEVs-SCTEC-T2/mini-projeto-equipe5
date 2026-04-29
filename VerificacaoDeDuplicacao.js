62d9fea167257d736a1818cc587a4a94d1292d58
cassianobigodudo <kakacassiano2010@hotmail.com>
2026-04-28 21:22:04 -0300

    refactor: refatora temDuplica para O(n) usando Set

--- VerificacaoDeDuplicacao.js
+++ VerificacaoDeDuplicacao.js
@@ -1,20 +1,37 @@
-numero = [1, 2, 3, 4, 5, 5]
+numeroLista = [1, 2, 3, 4, 5, 5, 4,3,2,5,645,123,342,52,23,12345,434,234,432,234,122]
 
 
-function temDuplica() {
+// function temDuplica() {
     
-for (let i = 0; i < numero.length; i++) {
+// for (let i = 0; i < numeroLista.length; i++) {
 
-    for (let j = i + 1; j < numero.length; j++) {
-            if (numero[i] === numero[j]) {
+//     for (let j = i + 1; j < numeroLista.length; j++) {
+//             if (numeroLista[i] === numeroLista[j]) {
 
-                console.log("Eu, o Robo, achei o número repetido.")
+//                 console.log("Eu, o Robo, achei o número repetido.")
 
-                return true; 
-            }
+//                 return true; 
+//             }
+//         }
+//     }
+//     return false;
+// }
+
+// temDuplica()
+
+
+function temDuplicatas(listagem){
+
+    const numerosVistos = new Set()
+    const numerosDuplicados = new Set()
+    for (const num of listagem){
+        if (numerosVistos.has(num)){
+            numerosDuplicados.add(num)
+        }else{
+            numerosVistos.add(num)
         }
     }
-    return false;
+    return { temDuplicatas: numerosDuplicados.size > 0, numerosDuplicados: [...numerosDuplicados] }
 }
 
-temDuplica()
+temDuplicatas(numeroLista)
